a3b96d64cd50a774ddb0e2b14ee7bdecf0617243
Dan Palmer <dpalmer@student.hive.fi>
2023-07-24 11:05:04 +0300

    vector normalization math

--- src/vector/v3d.c
+++ src/vector/v3d.c
@@ -6,13 +6,14 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 12:50:56 by dpalmer           #+#    #+#             */
-/*   Updated: 2023/07/17 12:54:36 by dpalmer          ###   ########.fr       */
+/*   Updated: 2023/07/24 10:54:59 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include "v3d.h"
 
+/* Takes 3 doubles as input and returns them as a vec3d struct*/
 t_v3d	init_v3d(double x, double y, double z)
 {
 	t_v3d	new;
@@ -21,4 +22,23 @@ t_v3d	init_v3d(double x, double y, double z)
 	new.y = y;
 	new.z = z;
 	return (new);
+}
+
+/* Takes a vec3d struct and returns separate struct of the normalized values */
+t_v3d	normalize_vector(t_v3d input)
+{
+	t_v3d	normal;
+	double	len;
+
+	len = vector_len(input);
+	normal.x = input.x / len;
+	normal.y = input.y / len;
+	normal.z = input.z / len;
+	return (normal);
+}
+
+/* Used to calculate the length of a vector in a 3D plane */
+double	vector_len(t_v3d input)
+{
+	return (sqrt(input.x * input.x + input.y * input.y + input.z * input.z));
 }
