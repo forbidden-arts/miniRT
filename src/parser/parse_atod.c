466e387468714ef7b75f8934b4e42174d115fab5
Sakari Salmi <sakarisalmi@hotmail.com>
2023-08-01 16:34:11 +0300

    'fixed' ft_atod

--- src/parser/parse_atod.c
+++ src/parser/parse_atod.c
@@ -6,17 +6,37 @@
 /*   By: ssalmi <ssalmi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 14:37:01 by dpalmer           #+#    #+#             */
-/*   Updated: 2023/08/01 15:01:29 by ssalmi           ###   ########.fr       */
+/*   Updated: 2023/08/01 16:30:34 by ssalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include "libft.h"
 
+// rm later
+#include <stdio.h>
+
+static size_t	ft_atod_get_digits(int fraction)
+{
+	size_t	digit_amount;
+
+	digit_amount = 0;
+	if (fraction == 0)
+		return (1);
+	while (fraction > 0)
+	{
+		digit_amount++;
+		fraction = fraction / 10;
+	}
+	return (digit_amount);
+}
+
 double	ft_atod(char *str)
 {
-	size_t	before;
-	size_t	after;
+	double	before;
+	double	after;
+	int		after_int;
+	size_t	digits_after;
 
 	before = (double)ft_atoi(str);
 	if (!ft_strchr(str, '.'))
@@ -24,8 +44,14 @@ double	ft_atod(char *str)
 	while (*str != '.')
 		str++;
 	str++;
-	after = (double)ft_atoi(str);
-	after /= pow(10, (int)log10(after) + 1);
+	after_int = ft_atoi(str);
+	after = (double)after_int;
+	if (after != 0)
+	{
+		digits_after = ft_atod_get_digits(after_int);
+		while (digits_after-- > 0)
+			after *= 0.1;
+	}
 	if (before < 0)
 		return (before - after);
 	return (before + after);
